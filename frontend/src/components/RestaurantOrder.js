807d31f115f58bcd6f1f68ddef410f8ca946b822
Jeremy <tan_kai_qun97@yahoo.com.sg>
2020-03-24 00:22:36 +0800

    Add cost and checkout button

--- frontend/src/components/RestaurantOrder.js
+++ frontend/src/components/RestaurantOrder.js
@@ -1,21 +1,39 @@
 import React from "react";
-import { Button, Icon } from "semantic-ui-react";
+import { Button, Icon, Label, Input } from "semantic-ui-react";
 
 function RestaurantOrder(props) {
   return (
     <>
-      <h1>{props.restaurant}</h1>
-      <Button
-        fluid
-        animated="fade"
-        onClick={() => props.setSelectedRestaurant("")}
-        color="red"
-      >
-        <Button.Content visible>
-          <Icon name="close" />
-        </Button.Content>
-        <Button.Content hidden content="Cancel order" />
-      </Button>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <h1>{props.restaurant}</h1>
+        <span>
+          <Input labelPosition="left" type="text" fluid>
+            <Label basic>$</Label>
+            <input disabled style={{ opacity: "1" }} />
+          </Input>
+
+          <Button.Group widths="2" style={{ minWidth: "20vw" }}>
+            <Button animated="vertical" color="teal">
+              <Button.Content hidden>Checkout</Button.Content>
+              <Button.Content visible>
+                <Icon name="shop" />
+              </Button.Content>
+            </Button>
+
+            <Button
+              animated="fade"
+              onClick={() => props.setSelectedRestaurant("")}
+              color="red"
+            >
+              <Button.Content visible>
+                <Icon name="close" />
+              </Button.Content>
+              <Button.Content hidden content="Cancel" />
+            </Button>
+          </Button.Group>
+        </span>
+      </div>
+
       <h1>To be implemented</h1>
     </>
   );
