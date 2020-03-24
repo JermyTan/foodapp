dd0291e4d40a800c8eed9c8e4ccfea071ff70e0c
sinteary <leewengyancynthia@gmail.com>
2020-03-25 00:54:01 +0800

    Completed CreateCustomer

--- backend/controllers/customers.js
+++ backend/controllers/customers.js
@@ -24,79 +24,40 @@ exports.getCustomers = async (req, response) => {
 // @acess   Private
 exports.createCustomer = async (req, response) => {
   const { email, name, cardnum } = req.body
-  console.log("req", req.body)
+  const createCustomerQuery =
+    `BEGIN;
+
+    SET CONSTRAINTS ALL DEFERRED;
+    INSERT INTO Users (email, name)
+    VALUES(${email}, ${name}) RETURNING id;
+
+    INSERT INTO Customers (id, rpoints, cardNum)
+    VALUES((SELECT currval('users_id_seq')), 0, (SELECT NULLIF(${cardnum}, 0))) RETURNING *; 
+
+    COMMIT;`
   try {
-    const wrapper = async () => db.query(`BEGIN`);
-    wrapper().then(
-      console.log("Begin finished executed")
+    const rows = await db.query(createCustomerQuery, async (err, result) => {
+      if (err) {
+        // console.error(err.stack);
+        console.log("ERROR", err.constraint)
+        if (err.constraint === 'users_email_key') {
+          response.status(400).json({ success: false, msg: 'Email already exists in DB.' })
+        } else {
+          response.status(500).json({ success: false, msg: 'Failed db query. Please try again.' })
+        }
 
-    );
-
-    //db.query(`BEGIN`)
-    // await db.query(`SET CONSTRAINTS ALL DEFERRED`)
-
-    // const queryCreateUser =
-    //   `INSERT INTO Users(email, name) VALUES($1, $2) RETURNING id`
-    // const queryCreateCustomer =
-    //   `INSERT INTO Customers(id, rpoints, cardNum)
-    //     VALUES($1, 0, (SELECT NULLIF($2, 0))) RETURNING *`
-
-    // const rows = await db.query(queryCreateUser, [email, name], async (err, result) => {
-    //   if (err) {
-    //     console.log(err.stack)
-    //     response.status(404).json({ success: false, msg: `Failed to create new user. Email may already exist in db.` })
-    //   } else {
-    //     console.log("result: ", result)
-    //     await db.query(queryCreateCustomer, [result.id, cardnum], async (err, result2) => {
-    //       if (err) {
-    //         response.status(404).json({ success: false, msg: `Failed to create new customer.` })
-    //       } else {
-    //         console.log(result2)
-    //         if (!result2.rows) {
-    //           response.status(404).json({ success: false, msg: `Failed to create new customer.` })
-    //         } else {
-    //           await db.query('COMMIT;')
-    //           console.log('Successfully created customer')
-    //           response.status(200).json({ success: true, msg: result2.rows[0] })
-    //         }
-    //       }
-    //     })
-    //   }
-    // })
-  } catch (e) {
-    console.log(e);
-    await db.query('ROLLBACK')
-    response.status(500).send('Failed db query. Please try again.')
+      } else {
+        console.log("result", result[2].rows, result[3].rows)
+        if (result[2].rows.id == result[3].rows.id)
+          response.status(200).json({ success: true, msg: "Created user/customer with id" })
+        else {
+          db.query('ROLLBACK');
+          response.status(404).json({ success: false, msg: `Failed to create customer.` })
+        }
+      }
+    })
+  } catch (err) {
+    console.log("ERROR", err)
+    response.status(500).json({ success: false, msg: 'Failed db query. Please try again.' })
   }
-
 }
-// const rows = await db.query(createCustomerQuery, (err, result) => {
-//       if (err) {
-//         console.error(err.stack)
-//         console.log("an error occured");
-//         response.status(400).json({ success: false, msg: `Something went wrong. Duplicate email?` })
-//       } else {
-//         console.log(result)
-//         if (!result.rows[0]) {
-//           response.status(404).json({ success: false, msg: `Failed to create new customer.` })
-//         } else {
-//           console.log('Successfully created  customer')
-//           response.status(200).json({ success: true, msg: result.rows[0] })
-//         }
-//       }
-//     });
-
-
- // let createCustomerQuery =
-  //   `BEGIN;
-
-  //   SET CONSTRAINTS ALL DEFERRED;
-  //   INSERT INTO Users (email, name)
-  //   VALUES(${email}, ${name});
-
-  //   INSERT INTO Customers (id, rpoints, cardNum)
-  //   VALUES((SELECT currval('users_id_seq')), 0, (SELECT NULLIF(${cardnum}, 0))); 
-
-  //   COMMIT;`
-
-  // const createUserQuery = `INSERT INTO USERS (email, name) VALUES ($1, $2) returning *`
