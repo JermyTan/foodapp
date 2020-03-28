06385457f4707f2dbcbdda6b18dfa03323dd3826
sinteary <leewengyancynthia@gmail.com>
2020-03-29 02:55:47 +0800

    Add backend for staff

--- backend/routes/api-routes.js
+++ backend/routes/api-routes.js
@@ -5,11 +5,14 @@ const orderRoute = require('./order-routes');
 const userRoute = require('./user-routes');
 const customerRoute = require('./customer-routes');
 const restaurantRoute = require('./restaurant-routes');
+const staffRoute = require('./staff-routes');
 
 // map URI to file route
 router.use('/orders', orderRoute);
 router.use('/users', userRoute);
 router.use('/customers', customerRoute);
 router.use('/restaurants', restaurantRoute);
+router.use('/staffs', staffRoute);
+
 
 module.exports = router;
