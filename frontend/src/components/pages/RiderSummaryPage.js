d755cd769046358dc7d8dba262c04f69a33976d2
Jeremy <tan_kai_qun97@yahoo.com.sg>
2020-03-31 23:26:58 +0800

    Add calendar

--- frontend/src/components/pages/RiderSummaryPage.js
+++ frontend/src/components/pages/RiderSummaryPage.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Menu, Container } from "semantic-ui-react";
+import { MonthRangeInput } from "semantic-ui-calendar-react";
 
 function RiderSummaryPage() {
+  const [selectedMonths, setSelectedMonths] = useState("");
+
   return (
     <main className="rider-summary-page">
       <Menu size="huge" style={{ opacity: 0 }}></Menu>
-      <br />
-      <br />
       <Container>
-        <h1>This is the rider summary page</h1>
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+          <h1 style={{ marginInlineEnd: "1rem" }}>Performance summary for</h1>
+          <MonthRangeInput
+            clearable
+            placeholder="Select month(s)"
+            popupPosition="bottom center"
+            onChange={(event, data) => {
+              setSelectedMonths(data.value);
+              console.log(data.value);
+            }}
+            dateFormat="MM/YYYY"
+            value={selectedMonths}
+            closable
+            maxDate={new Date()}
+          />
+        </div>
       </Container>
     </main>
   );
