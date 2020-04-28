1d6450ebbeef13563d2e0fc2c47968e0b70d8f7a
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-04-28 19:51:06 +0800

    Add sample sql function and triggers

--- backend/sql/query-fn.sql
+++ backend/sql/query-fn.sql
@@ -1,19 +1,43 @@
-DROP FUNCTION IF EXISTS getrestaurants();
+-- Sample PL/PGSQL function
+DROP FUNCTION IF EXISTS getrestaurants() CASCADE;
+DROP TYPE IF EXISTS rest_tuple CASCADE;
 
 CREATE TYPE rest_tuple AS (rname text, cat text);
 
 CREATE OR REPLACE FUNCTION getrestaurants()
 RETURNS SETOF rest_tuple AS $$ 
-declare 
+DECLARE 
 	result rest_tuple; 
-begin
-	for result in
-		select distinct s.rname, f.cat from food f NATURAL JOIN sells s
-	loop
+BEGIN
+	for result IN
+		SELECT DISTINCT r.rname, f.cat 
+		FROM restaurants r
+		left outer join
+		(food f NATURAL JOIN sells s)
+		on r.rname = s.rname
+	LOOP
 		RETURN next result;
-	end loop;
-end; 
-$$ language plpgsql;
-
+	END LOOP;
+END; 
+$$ LANGUAGE plpgsql;
 -- call fn in backend like this:
 -- select * from getrestaurants()
+
+-- Sample PL/PGSQL trigger
+DROP FUNCTION IF EXISTS getrestaurants() CASCADE;
+
+CREATE OR REPLACE FUNCTION public.pt_check_dur()
+RETURNS TRIGGER
+AS $function$
+DECLARE
+	dur INTEGER;
+BEGIN
+    SELECT(etime - stime) INTO dur
+        FROM wws
+        WHERE (etime - stime) >= 4;
+    IF dur IS NOT NULL THEN
+        RAISE exception 'Rider id: % is working for % hours', wws.id, dur;
+    END IF;
+    RETURN NULL;
+END;
+$function$ LANGUAGE plpgsql
