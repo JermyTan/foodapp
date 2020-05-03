4964683d7cde493b539edfa7d41316a8df671590
Jeremy <tan_kai_qun97@yahoo.com.sg>
2020-05-04 01:51:02 +0800

    Add loader

--- frontend/src/components/customers/AllRestaurants.js
+++ frontend/src/components/customers/AllRestaurants.js
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { Card, Search, Input } from "semantic-ui-react";
+import { Card, Search, Input, Segment } from "semantic-ui-react";
 import RestaurantCard from "./RestaurantCard";
 import "styles/AllRestaurants.scss";
 import Axios from "axios";
 
 function AllRestaurants(props) {
   const [restaurantsData, setRestaurantData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   const url = `http://localhost:5000/api/restaurants`;
   useEffect(() => {
     Axios.get(url)
       .then((response) => {
         console.log("response", response.data);
         setRestaurantData(response.data);
+        setLoading(false);
       })
       .catch((error) => {
         console.log("Error retrieving past orders:", error);
@@ -46,22 +49,27 @@ function AllRestaurants(props) {
       </div>
 
       {/* {props.location && ( */}
-      <Card.Group>
-        {restaurantsData.map((value, index) => {
-          return (
-            <RestaurantCard
-              key={index}
-              restaurant={value}
-              setSelectedRestaurant={props.setSelectedRestaurant}
-            />
-          );
-        })}
-
-        {/* <RestaurantCard setSelectedRestaurant={props.setSelectedRestaurant} />
-          <RestaurantCard setSelectedRestaurant={props.setSelectedRestaurant} />
-          <RestaurantCard setSelectedRestaurant={props.setSelectedRestaurant} />
-          <RestaurantCard setSelectedRestaurant={props.setSelectedRestaurant} /> */}
-      </Card.Group>
+      {loading ? (
+        <Segment
+          size="massive"
+          basic
+          placeholder
+          loading={loading}
+          textAlign="center"
+        />
+      ) : (
+        <Card.Group>
+          {restaurantsData.map((value, index) => {
+            return (
+              <RestaurantCard
+                key={index}
+                restaurant={value}
+                setSelectedRestaurant={props.setSelectedRestaurant}
+              />
+            );
+          })}
+        </Card.Group>
+      )}
     </>
   );
 }
